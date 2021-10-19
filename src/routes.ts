f96e7432e706ac3347b870e440da119fc518293c
Vinícius Quadrado <68359941+viniciuSquare@users.noreply.github.com>
2021-10-19 01:36:36 -0300

    Initiating backend structure

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+export{ router }
